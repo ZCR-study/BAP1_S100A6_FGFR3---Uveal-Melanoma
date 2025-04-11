849a033d7dbcf2c1d47fc9c3d7290e02d6417bd4
ZCR-study <zcr200109@gmail.com>
2025-04-11 16:40:14 +0800

    Add files via upload

---
+++ bulk_RNA_and_Array/01_TCGA_gene_expression_comparative analysis.R
@@ -0,0 +1,98 @@
+###########Comparative analysis of gene expression levels between two groups#############################
+##Environment setup and clearing
+Sys.setenv(LANGUAGE = "en")
+options(stringsAsFactors = FALSE)
+rm(list=ls());gc()
+setwd("./bulk_RNA_and_Array")
+
+##between "Disomy chr3" group and "Monosomy chr3" group
+
+# --- Load Data ---
+load("./processed_data/TCGA/TCGA-UVM_SNP.Rdata")
+load("./processed_data/TCGA/TCGA_UM_survival_TPM.Rdata")
+
+# --- Load Required Libraries ---
+library(stringr)
+library(dplyr)
+library(ggplot2)
+library(ggpubr)
+
+# --- Process BAP1 Mutation Data ---
+BAP1 <- data[data$Hugo_Symbol=="BAP1",]
+BAP1 <- BAP1[,c("Tumor_Sample_Barcode","Hugo_Symbol")]
+BAP1$Tumor_Sample_Barcode <- str_sub(BAP1$Tumor_Sample_Barcode,1,12)
+names(BAP1)[2] <- "mutation"
+BAP1$mutation <- paste0(BAP1$mutation ," MUT")
+
+# --- Process Expression Data ---
+exp <- exprSet
+exp <- exp[!duplicated(rownames(exp)),]
+exp <- na.omit(exp)
+exp <- log2(exp+1)
+
+# --- Merge Mutation Status into Metadata ---
+meta <- left_join(meta,BAP1,by=c("ID"="Tumor_Sample_Barcode"))
+meta$mutation <- ifelse(!is.na(meta$mutation),"BAP1 MUT","BAP1 WT")
+
+# --- Prepare Data for BAP1 WT vs MUT Comparison ---
+group_list=meta$mutation
+#设置参考水平，对照在前，处理在后
+group_list= factor(group_list,
+                   levels = c("BAP1 WT","BAP1 MUT"))
+table(group_list)
+
+identical(meta$ID,str_sub(colnames(exp),1,12))
+
+S100A6=exp["S100A6",]
+
+log2_TPM_S100A6 <- t(S100A6)
+
+group <- group_list
+dd <- data.frame(group,log2_TPM_S100A6)
+#dd
+#str(dd)
+dd$group = as.factor(dd$group)
+
+# --- Plot S100A6 Expression: BAP1 WT vs MUT ---
+ggplot(dd,
+       mapping=aes(x=group,y=S100A6, fill=group))+
+  geom_jitter(width = 0.15, shape=21, size=3, color="#252a32") + 
+  geom_boxplot(staplewidth = 0.5, width=0.75, 
+               outliers = FALSE, alpha=0.75) +
+  scale_fill_manual(values = c("#1c8041","#eb5354"),
+                    guide = "none")  +   ylab("log2 TPM S100A6")+
+  stat_compare_means(method = "t.test") + 
+  scale_y_continuous(limits = c(6,13),breaks = seq(6,12,2),expand = c(0,0.2)) +
+  theme_classic2()
+
+ggsave("./Rplot/S100A6_in_BAP1_WT_vs_BAP1_MUT.pdf",width = 3.85,height = 3.67,
+       units = "in",dpi = 600)
+
+dev.off()
+
+# --- Prepare Data for Disomy vs Monosomy Comparison ---
+meta$chr <- ifelse(str_detect(meta$chromosome,"3"),"Monosomy 3","Disomy 3")
+group_list=meta$chr
+group_list= factor(group_list,
+                   levels = c("Disomy 3","Monosomy 3"))
+table(group_list)
+group <- group_list
+dd <- data.frame(group,log2_TPM_S100A6)
+#dd
+#str(dd)
+dd$group = as.factor(dd$group)
+
+# --- Plot S100A6 Expression: Disomy vs Monosomy ---
+ggplot(dd,
+       mapping=aes(x=group,y=S100A6, fill=group))+
+  geom_jitter(width = 0.15, shape=21, size=3, color="#252a32") + 
+  geom_boxplot(staplewidth = 0.5, width=0.75, 
+               outliers = FALSE, alpha=0.75) +
+  scale_fill_manual(values = c("#1c8041","#eb5354"),
+                    guide = "none")  +   ylab("log2 TPM S100A6")+
+  stat_compare_means(method = "t.test") + 
+  scale_y_continuous(limits = c(6,13),breaks = seq(6,12,2),expand = c(0,0.2)) +
+  theme_classic2()
+
+ggsave("./Rplot/S100A6_in_Disomy chr3_vs_Monosomy chr3.pdf",width = 3.85,height = 3.67,
+       units = "in",dpi = 600)
---
+++ bulk_RNA_and_Array/05_GSE22138_data_process.R
@@ -0,0 +1,111 @@
+###########GSE22138_data_process#############################
+##Environment setup and clearing
+Sys.setenv(LANGUAGE = "en")
+options(stringsAsFactors = FALSE)
+rm(list=ls());gc()
+setwd("./bulk_RNA_and_Array")
+
+# --- Load Required Libraries ---
+library(limma)
+library(dplyr)
+library(tidyverse)
+library(dplyr)
+library(survival) # Survival Analysis
+library(survminer) 
+
+# --- Load Processed Data ---
+load("./processed_data/GSE22138/GSE22138_exp_pd.Rdata")
+
+# --- Prepare Data for Differential Expression (Metastasis vs. Non-metastasis) ---
+group_list=ifelse(str_detect(pd$`metastasis:ch1`,"no"),'Non-metastasis',"Metastasis")
+
+group_list = factor(group_list,
+                    levels = c('Non-metastasis',"Metastasis"))
+table(group_list)
+
+# --- Limma Differential Expression Analysis Pipeline ---
+design=model.matrix(~group_list)
+fit=lmFit(exp,design)
+fit=eBayes(fit)
+deg=topTable(fit,coef=2,number = Inf)
+
+
+deg <- mutate(deg,probe_id=rownames(deg))
+head(deg)
+
+
+logFC_t=0.5
+P.Value_t = 0.05
+k1 = (deg$P.Value < P.Value_t)&(deg$logFC < -logFC_t)
+k2 = (deg$P.Value < P.Value_t)&(deg$logFC > logFC_t)
+change = ifelse(k1,"down",ifelse(k2,"up","stable"))
+deg <- mutate(deg,change)
+table(deg$change)
+
+save(deg,file = "./processed_data/GSE22138/GSE22138_deg.Rdata")
+
+
+
+# --- Survival Analysis based on Chromosome 3 Status ---
+rm(list = ls());gc()
+
+# --- Reload Data for Survival Analysis ---
+load("./processed_data/GSE22138/GSE22138_exp_pd.Rdata")
+
+# --- Prepare Metadata for Survival Analysis ---
+pd <- pd[pd$`chromosome 3 status:ch1`!="NA",]
+pd <- pd[pd$`chromosome 3 status:ch1`!="partial monosomy",]
+meta <- pd
+meta$time <- as.numeric(meta$`months to endpoint:ch1`)
+meta$event <- ifelse(meta$`metastasis:ch1`=="yes",1,0)
+meta$group <- factor(meta$`chromosome 3 status:ch1`,levels =  c("disomy","monosomy"))
+colnames(meta)
+
+
+
+# --- Fit Kaplan-Meier Survival Model ---
+fit <- survfit(Surv(time, event) ~ group, data = meta)
+
+# --- Generate and Save Kaplan-Meier Plot ---
+ggsurvplot(fit,
+           data = meta, 
+           legend.title = "Group",
+           pval =TRUE, 
+           conf.int = TRUE,
+           risk.table = F, # Add risk table
+           tables.height = 0.2,
+           tables.theme = theme_cleantable(),
+           palette = c( "#1c8041","#eb5354"),
+           ggtheme = theme_bw()
+)
+
+ggsave(filename = "./Rplot/chromosome-GSE22138-km_plot.pdf",width = 3.5,height = 3.5)
+
+dev.off()
+
+# --- Generate and Save Cumulative Hazard/Event Plot ---
+ggsurvplot(fit,
+           fun = "event", # 累计风险概率
+           title = NULL,
+           risk.table = F, # 风险表
+           # risk.table.col = "strata", # 根据分层更改风险表颜色
+           pval = TRUE, # p值
+           pval.method=TRUE,
+           pval.coord = c(62,1), #p 值位置
+           pval.method.coord= c(30,1),
+           censor.shape = "", # 删失值标记
+           surv.scale = "percent", # 百分比显示生存率
+           #legend =c(0.9, 0.1), 
+           legend.title ="Group",
+           legend="top",
+           legend.labs = c("disomy","monosomy"),
+           xlim = c(0, 100), # 横坐标轴范围，相当于局部放大
+           break.time.by = 20, # 横坐标刻度
+           ylim = c(0, 1), # 这里只为演示用，y轴0~200%毫无意义。
+           palette = c( "#1c8041","#eb5354"), # nejm配色
+           data = meta,
+           conf.int = T,
+           ggtheme = theme_bw()
+)
+
+ggsave(filename = "./Rplot/chromosome-GSE22138-cumulative_plot.pdf",width = 3.5,height = 3.5)
